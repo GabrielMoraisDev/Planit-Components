ec7ae60828636fa11bee3c857638b974c4801ad9
Gabriel-Morais-TI <leiteetec@gmail.com>
2025-04-02 23:39:40 -0300

    Google Analytcs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Dancing_Script, Nunito } from "next/font/google";
 import { DataProvider } from "@/app/_context/data";
 import "./globals.css";
+import Script from "next/script";
 
 const dancingScript = Dancing_Script({
   variable: "--font-dancing-script",
@@ -25,6 +26,19 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${fontNunito.variable} ${dancingScript.variable}`}>
       <body className="font-nunito duration-300">
+        {/* Google Analytics Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-5CV2BCWDQ2"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-5CV2BCWDQ2');
+          `}
+        </Script>
         <DataProvider>{children}</DataProvider>
       </body>
     </html>
